192717c4aa4445b33e8d941b1aa7e7cb8a7928cf
bindreams <28830446+bindreams@users.noreply.github.com>
2026-04-19 20:22:06 +0200

    Add auto-reloading capabilities to the nginx image

--- nginx/reload.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# This is a cronjob. Create a symlink to this script in /etc/cron.daily
-set -euxo pipefail
-cd $(dirname $(realpath "$0"))
-
-docker compose exec nginx nginx -s reload
